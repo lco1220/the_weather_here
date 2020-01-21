66c7bd188c07ec0e35c984950c53b735202b6ac6
lco1220 <leorieleco@gmail.com>
2020-01-21 15:06:11 +0800

    Edit port

--- node.js
+++ node.js
@@ -6,8 +6,9 @@ require('dotenv').config();
 // console.log(process.env)
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.listen(3010, () => console.log('Express running'));
+app.listen(port, () => console.log(`Running at port ${port}`));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
